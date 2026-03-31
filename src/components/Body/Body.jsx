0e51f810dceb2e437bbd21f3448d3691613097ca
Martin <coxmartin877@gmail.com>
2026-03-30 22:15:58 -0600

    agregacion de imagenes para mostrar dinamicamente, despues se hace el cambio para poder mostrarlas mejor y mejorar el rendimiento

--- src/components/Body/Body.jsx
+++ src/components/Body/Body.jsx
@@ -1,9 +1,14 @@
 import Especie from "./Especie/Especie"
+import especies from '../../store/especies.json'
 
 function Body() {
   return (
     <div className="h-dvh">
-        <Especie />
+      {
+        especies.map((especie, index) => (
+          <Especie key={index} imagen={especie.especie} especie={especie.nombre} descripcion={especie.descripcion} />
+        ))
+      }
     </div>
   )
 }
--- src/components/Body/Especie/Especie.jsx
+++ src/components/Body/Especie/Especie.jsx
@@ -1,16 +1,15 @@
-function Especie() {
+function Especie({especie,  descripcion, imagen}) {
   return (
     <div className="h-80 mt-20 px-8 w-full flex items-center justify-center relative">
-      <div className="w-full h-full relative image">
+      <div className={`w-full h-full relative rounded-md image ${imagen}`}>
       </div>
 
       <div className="flex justify-center text-white gap-4 items-center absolute flex-col w-1/2">
-        <h2 className="text-5xl font-bold">Especie</h2>
-        <p className="text-xl">Descripcion de la especie</p>
+        <h2 className="text-5xl font-bold">{especie}</h2>
+        <p className="text-xl">{descripcion}</p>
       </div>
     </div>
   )
 }
 
 export default Especie
-  
